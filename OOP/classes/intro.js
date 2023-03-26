e1a926b34b60faaac5edb88bb3426fca9f33b2a8
shruti248 <shrutiunadkat5@gmail.com>
2023-03-26 18:58:53 +0530

    Classes Practice

--- OOP/classes/intro.js
+++ OOP/classes/intro.js
@@ -26,3 +26,25 @@ console.log(authorInfo);
 
 // Using the instanceof operator to check an object's type
 console.log("is bookObj instance of Book ? --------" , bookObj instanceof Book)
+
+
+class Task {
+    static taskList = [];
+
+    static printTaskList() {
+      console.log(this.taskList);
+    }
+
+    static addTask(task) {
+      this.taskList.push(task);
+    }
+
+    constructor(taskDetail) {
+      this.taskDetail = taskDetail;
+      Task.addTask(this);
+    }
+  }
+
+  const laundry = new Task('laundry');
+  console.log(Task.printTaskList());
+  console.log(laundry.taskList);
