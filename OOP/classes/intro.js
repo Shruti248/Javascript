f51051282f193af75b231dc5e85c66a7843304ce
shruti248 <shrutiunadkat5@gmail.com>
2023-03-26 18:17:43 +0530

    OOP : Classes

---
+++ OOP/classes/intro.js
@@ -0,0 +1,28 @@
+// Defiing a class woith constructor
+
+// REMEMBER : Class names start with capital letters
+class Book{
+    constructor(title , series , author ){
+        this.title = title;
+        this.series = series;
+        this.author = author;
+    }
+
+    // Defining Methods
+    getAuthorInformtion(){
+        return `${this.author}`
+    }
+}
+
+// Instanace of the class : NEW KEYWORD
+const bookObj = new Book('This is the title' , 'This is the series' , 'This is the author');
+
+console.log(bookObj);
+
+// Accessing Methods of the class
+const authorInfo = bookObj.getAuthorInformtion();
+console.log(authorInfo);
+
+
+// Using the instanceof operator to check an object's type
+console.log("is bookObj instance of Book ? --------" , bookObj instanceof Book)
