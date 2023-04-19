dfea823b51cf4682d98ecef3d75e73cfb05fb810
shruti248 <shrutiunadkat5@gmail.com>
2023-04-19 18:03:21 +0530

    Express Router 2

--- Express/practice-for-week-10-express-router-main/server/routes/colors.js
+++ Express/practice-for-week-10-express-router-main/server/routes/colors.js
@@ -9,6 +9,12 @@ router.get('/' , (req , res) => {
 router.get('/:name' , (req , res) => {
     res.json('GET /colors/:name' + `  GET /colors/${req.params.name}`)
 })
+router.post('/:name/css-styles' , (req , res) => {
+    res.json("POST /colors/:name/css-styles")
+})
+router.delete('/:name/css-styles/:style' , (req , res) => {
+    res.json("DELETE /:name/css-styles/:style")
+})
 
 
 module.exports = router;
